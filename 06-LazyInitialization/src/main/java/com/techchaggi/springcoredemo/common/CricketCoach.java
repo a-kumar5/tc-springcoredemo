4a8c8bcdb78862f1dbddbada86fbee9e4104310a
a-kumar5 <ayush.1306.kumar@gmail.com>
2023-07-23 14:10:45 +0530

    Code added to change bean scope from singleton to prototype

--- 06-LazyInitialization/src/main/java/com/techchaggi/springcoredemo/common/CricketCoach.java
+++ 06-LazyInitialization/src/main/java/com/techchaggi/springcoredemo/common/CricketCoach.java
@@ -1,11 +1,14 @@
 package com.techchaggi.springcoredemo.common;
 
-import org.springframework.context.annotation.Primary;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 // @Component annotation marks the class as spring bean and makes it available for dependency injection
-@Component
+
 //@Primary
+@Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class CricketCoach implements Coach {
     
 	public CricketCoach() {
